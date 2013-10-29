01e2199797f02e33ab7b974015a4c6937d2202cc
NoNameSheep <mingyang.wu@edoctor.cn>
2013-10-29 11:15:56 +0800

    fix bug

--- fal.js
+++ fal.js
@@ -1,16 +1,22 @@
-(function () {
+(function() {
     'use strict';
 
-    function Item (data, prev, next) {
+    function Item(data, prev, next) {
         this.data = data;
         this.prev = prev || null;
-        if (prev) { prev.next = this; }
+        if (prev) {
+            prev.next = this;
+        }
         this.next = next || null;
-        if (next) { next.prev = this; }
+        if (next) {
+            next.prev = this;
+        }
     }
 
-    function Fal () {
-        if (! this instanceof Fal) { return new Fal(); }
+    function Fal() {
+        if (!this instanceof Fal) {
+            return new Fal();
+        }
         this.head = null;
         this.tail = null;
         this.length = 0;
@@ -21,15 +27,17 @@
 
     Fal.prototype = {
 
-        push: function (data) {
+        push: function(data) {
             this.tail = new Item(data, this.tail, null);
             this.head = this.head || this.tail;
             this.current = this.head;
             this.length++;
         },
 
-        pop: function () {
-            if (this.length === 0) { return undefined; }
+        pop: function() {
+            if (this.length === 0) {
+                return undefined;
+            }
             var ret = this.tail;
             this.tail = ret.prev;
             if (--this.length === 0) {
@@ -41,27 +49,29 @@
             return ret.data;
         },
 
-        shift: function () {
-           if (this.length === 0) { return undefined; }
-           var ret = this.head;
-           this.head = ret.next;
-           if (--this.length === 0) {
-               this.tail = null;
-           } else {
-               this.head.prev = null;
-           }
-           this.current = this.head;
-           return ret.data;
+        shift: function() {
+            if (this.length === 0) {
+                return undefined;
+            }
+            var ret = this.head;
+            this.head = ret.next;
+            if (--this.length === 0) {
+                this.tail = null;
+            } else {
+                this.head.prev = null;
+            }
+            this.current = this.head;
+            return ret.data;
         },
 
-        unshift: function (data) {
+        unshift: function(data) {
             this.head = new Item(data, null, this.head);
             this.tail = this.tail || this.head;
             this.current = this.head;
             this.length++;
         },
 
-        indexOf: function (data) {
+        indexOf: function(data) {
             this.index = 0;
             this.current = this.head;
             while (this.current) {
@@ -77,26 +87,81 @@
             return -1;
         },
 
-        slice: function (start, end) {
-            var ret = new Fal ();
-            var i = 0;
-            start = start ? 0 : +start;
-            if (start >= this.length || start === +end) {
-                return ret;
+        _take: function(i) {
+            // internal use: change current
+            // return null if failed
+            if (i >= this.length || -i > this.length) {
+                return null;
             }
-            if (start === this.index) {
-                ret.head = this.current;
-                i = start + 1;
+            if (i === this.current || this.current === i + this.length) {
+                return this.current;
+            }
+            if (i >= 0) {
+                this.index = 0;
+                this.current = this.head;
+                while (i-- > 0) {
+                    this.current = this.current.next;
+                    this.index++;
+                }
             } else {
-                ret.head = this.head;
-                while(i++ <= start) {
-                    ret.head = ret.head.next;
+                this.index = this.length - 1;
+                this.current = this.tail;
+                while (++i < 0) {
+                    this.current = this.current.prev;
+                    this.index--;
                 }
             }
-            end = +end || this.length;
-            ret.tail = ret.current = ret.head;
-            while (i++ < end && ret.tail.next) {
-                ret.tail = ret.tail.next;
+
+            return this.current;
+        },
+
+        slice: function(start, end) {
+            start = start ? +start : 0;
+            end = end || this.length;
+
+            // normalize negative
+            start += (start < 0 ? this.length : 0);
+            end += (end < 0 ? this.length : 0);
+
+            var tmp = new Fal();
+            tmp.length = end - start;
+            tmp.head = this._take(start);
+            this.current = this.head;
+            this.index = 0;
+            return tmp.toArray();
+        },
+
+        remove: function(i) {
+            if (this._take(i) === null) {
+                return;
+            }
+            var current = this.current;
+            if (current.prev) {
+                current.prev.next = current.next;
+            } else {
+                this.head = current.next;
+            }
+            if (current.next) {
+                current.next.prev = current.prev;
+            } else {
+                this.tail = current.prev;
+            }
+            this.current = this.head;
+            this.index = 0;
+        },
+
+        addArray: function(arr) {
+            for (var i = arr.length - 1; i >= 0; --i) {
+                this.push(arr[i]);
+            }
+        },
+
+        toArray: function() {
+            var ret = [], i = 0,
+                current = this.head;
+            while(i++ < this.length && current) {
+              ret.push(current.data);
+              current = current.next;
             }
             return ret;
         }
